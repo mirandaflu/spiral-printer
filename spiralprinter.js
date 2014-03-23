494dafe0d21c41344d3734cdb08380aa0b9d0b54
mirandaflu <miranda.fluh@gmail.com>
2014-03-22 18:26:26 -0600

    did some cleanup, fixed issue #1

--- spiralprinter.js
+++ spiralprinter.js
@@ -6,51 +6,36 @@ var spiral = {
                 '0': '0'
             }
         };
-        this.nav.location = {
+        this.location = {
             'x': 1,
             'y': 0
         };
-        this.nav.direction.x = 0;
-        this.nav.direction.y = 1;
-    },
-    
-    'nav': {
-        'direction': {
+        this.direction = {
             'x': 0,
             'y': 1
-        },
-        'next_direction': function(old_x, old_y) {
-            // cycles [1,0] -> [0,1] -> [-1,0] -> [0,-1]
-            var new_x = (this.direction.x !== 0)? 0: (this.direction.y > 0)? -1: 1;
-            var new_y = (this.direction.y !== 0)? 0: (this.direction.x < 0)? -1: 1;
-            return {'x': new_x, 'y': new_y};
-        },
-        'location': {
-            'x': 1,
-            'y': 0
-        }
+        };
+    },
+    
+    'next_direction': function() {
+        // cycles [1,0] -> [0,1] -> [-1,0] -> [0,-1]
+        var new_x = (this.direction.x !== 0)? 0: (this.direction.y > 0)? -1: 1;
+        var new_y = (this.direction.y !== 0)? 0: (this.direction.x < 0)? -1: 1;
+        return {'x': new_x, 'y': new_y};
     },
     
     'insert': function(insertee) {
         
         //insert insertee
-        if (typeof(this.data[this.nav.location.y]) == 'undefined') this.data[this.nav.location.y] = {};
-        this.data[this.nav.location.y][this.nav.location.x] = insertee.toString();
-        
-        //console.log('inserted',insertee.toString(),'at',this.nav.location.x, this.nav.location.y);
+        if (typeof(this.data[this.location.y]) == 'undefined') this.data[this.location.y] = {};
+        this.data[this.location.y][this.location.x] = insertee.toString();
         
         //change direction if the next direction is clear
-        var nextdir = this.nav.next_direction(this.nav.direction.x, this.nav.direction.y);
-        if ((typeof(this.data[this.nav.location.y+nextdir.y]) == 'undefined') || (typeof(this.data[this.nav.location.y+nextdir.y][this.nav.location.x+nextdir.x]) == 'undefined')) {
-            //console.log('changing direction');
-            //console.log(this.nav.direction.x, this.nav.direction.y, ' to ', nextdir.x, nextdir.y);
-            this.nav.direction.x = nextdir.x;
-            this.nav.direction.y = nextdir.y;
-        }
+        if ((typeof(this.data[this.location.y+this.next_direction().y]) == 'undefined') || (typeof(this.data[this.location.y+this.next_direction().y][this.location.x+this.next_direction().x]) == 'undefined'))
+            this.direction = this.next_direction();
         
         //update location
-        this.nav.location.x += this.nav.direction.x;
-        this.nav.location.y += this.nav.direction.y;
+        this.location.x += this.direction.x;
+        this.location.y += this.direction.y;
         
     },
     
@@ -65,22 +50,21 @@ var spiral = {
     
     'print': function() {
         
-        //store direction
-        var dir = {'x': this.nav.direction.x, 'y': this.nav.direction.y};
-        
-        //insert spaces until direction changes
-        while (this.nav.direction.x == dir.x && this.nav.direction.y == dir.y)
+        //insert spaces until you hit a corner
+        var checkspot = {};
+        while (1) {
+            checkspot.x = this.location.x + this.next_direction().x;
+            checkspot.y = this.location.y + this.next_direction().y;
+            if (typeof(this.data[checkspot.y]) == 'undefined') break;
+            if (typeof(this.data[checkspot.y][checkspot.x]) == 'undefined') break;
             this.insert(' ');
-        
-        //take out last space
-        delete this.data[this.nav.location.y][this.nav.location.x];
+        }
         
         //print
         var result = '';
-        var i = 0;
+        var i = 0, j = 0;
         while (typeof(this.data[i]) != 'undefined') { --i; }
         ++i;
-        var j = 0;
         while (typeof(this.data[i][j]) != 'undefined') { --j; }
         ++j;
         var startj = j;
